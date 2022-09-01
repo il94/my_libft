9e7ec2ebeaf0d9ddb86715765d7e4195bca40714
Ilyes LANDOLSI <ilandols@e1r2p6.clusters.42paris.fr>
2022-09-01 18:30:11 +0200

    lstcorrect

--- Makefile
+++ Makefile
@@ -60,6 +60,7 @@ LISTS_SRC = ft_lstnew.c \
 			ft_lstclear.c \
 			ft_lstiter.c \
 			ft_lstmap.c	\
+			ft_lstcorrect.c
 
 LISTS_OBJ = $(LISTS_SRC:.c=.o)
 
--- linked_lists/linked_lists.h
+++ linked_lists/linked_lists.h
@@ -30,5 +30,6 @@ void		ft_lstdelone(t_list *lst, void (*del)(void *));
 void		ft_lstclear(t_list **lst, void (*del)(void *));
 void		ft_lstiter(t_list *lst, void (*f)(void *));
 t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+int			ft_lstcorrect(t_list *lst, int size);
 
 #endif
